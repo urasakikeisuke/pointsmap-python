0e107edc3a9647376ff080efe7e9813b628d5ecb
shikishima-TasakiLab <55086542+shikishima-TasakiLab@users.noreply.github.com>
2021-06-03 18:38:48 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from skbuild.command.build_ext import build_ext
 setup(
     name='pointsmap',
     version='1.0.0',
-    description='`http://git-docker.tasakilab:5050/shikishima/pointsmap-python/wiki`',
-    long_description='`http://git-docker.tasakilab:5050/shikishima/pointsmap-python/wiki`',
+    description='`https://github.com/shikishima-TasakiLab/pointsmap-python`',
+    long_description='`https://github.com/shikishima-TasakiLab/pointsmap-python`',
     author='Junya Shikishima',
     author_email='160442065@ccalumni.meijo-u.ac.jp',
-    url='http://git-docker.tasakilab:5050/shikishima/pointsmap-python',
+    url='https://github.com/shikishima-TasakiLab',
     license='',
     packages=['pointsmap'],
 )
